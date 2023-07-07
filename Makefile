a6421c8d1ea8e74388e06e35ae75923054a0beda
crrips <ahadzhan.piloian@gmail.com>
2023-07-07 14:29:50 +0400

    readline added

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME = minishell
+
+SRC = main.c
+OBJ = $(SRC:.c=.o)
+
+FLAGS = -Wall -Werror -Wextra
+
+LINKERS = -lreadline
+
+HEADERS = minishell.h
+
+RED 	= \033[31m
+YELLOW 	= \033[33m
+GREEN 	= \033[32m
+END 	= \033[0m
+
+%.o:%.c $(HEADERS)
+	@cc $(FLAGS) -c $< -o $@
+
+all: $(NAME)
+$(NAME):$(OBJ)
+	@cc $(FLAGS) $(LINKERS) $(OBJ) -o $@
+	@echo "$(GREEN)Executable file $(NAME) was compiled!$(END)"
+
+clean:
+	@rm -f $(OBJ)
+	@echo "$(YELLOW)Object files were deleted!$(END)"
+	
+fclean: clean
+	@rm -f $(NAME)
+	@echo "$(YELLOW)Executable file was deleted!$(END)"
+
+re:	fclean all
+	@echo "$(GREEN)Project was reloaded!$(END)"
+
+.PHONY: all clean fclean re
---
+++ minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 15:30:17 by user              #+#    #+#             */
+/*   Updated: 2023/07/02 14:19:06 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/time.h>
+
+#endif
